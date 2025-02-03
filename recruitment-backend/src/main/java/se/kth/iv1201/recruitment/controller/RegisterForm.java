6806fd3421674efaea378d7ce6425e5ef4e7ac0e
bhnur <bhnur@kth.se>
2025-02-03 15:19:01 +0100

    Added validation of person number + gitignore updated to ignore all vscode stuff

--- recruitment-backend/src/main/java/se/kth/iv1201/recruitment/controller/RegisterForm.java
+++ recruitment-backend/src/main/java/se/kth/iv1201/recruitment/controller/RegisterForm.java
@@ -2,6 +2,7 @@ package se.kth.iv1201.recruitment.controller;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 
 
@@ -16,10 +17,15 @@ public class RegisterForm {
     @NotBlank(message = "Username is left empty ")
     private String username;
     
+    
     @NotBlank(message = "Password field is empty")
     private String password;
 
+
+    @NotBlank(message = "Firstname field is empty")
     private String firstname;
+
+    @NotBlank(message = "Lastname field is empty")
     private String lastname;
     
    
@@ -27,6 +33,7 @@ public class RegisterForm {
     @NotBlank(message = "Email field is empty")
     private String email;
 
+    @Pattern(regexp = "\\d{8}-\\d{4}", message = "invalid person number format, Expected:YYYYMMDD-NNNN")
     @NotBlank(message = "person number field is empty")
     private String personNumber;
 
