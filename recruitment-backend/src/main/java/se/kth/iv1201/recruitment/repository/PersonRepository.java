b4bec30df494c67374893e0c37a811e3ed61854e
jontetk <jonathan@post.ax>
2025-02-06 17:17:44 +0200

    Made tests transactional

--- recruitment-backend/src/main/java/se/kth/iv1201/recruitment/repository/PersonRepository.java
+++ recruitment-backend/src/main/java/se/kth/iv1201/recruitment/repository/PersonRepository.java
@@ -5,6 +5,7 @@ package se.kth.iv1201.recruitment.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import se.kth.iv1201.recruitment.model.Person;
 
 /*
@@ -12,7 +13,7 @@ import se.kth.iv1201.recruitment.model.Person;
  */
 
 @Repository
-@org.springframework.transaction.annotation.Transactional(propagation = Propagation.MANDATORY)
+@Transactional(propagation = Propagation.MANDATORY)
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
   /**
